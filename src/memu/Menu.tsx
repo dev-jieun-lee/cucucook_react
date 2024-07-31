faa431ddb33ee0d41fd762962aafbb943273d4ea
eunhye lee <eeunh2@gmail.com>
2024-07-31 17:56:07 +0900

    테스트 페이지 추가

--- src/memu/Menu.tsx
+++ src/memu/Menu.tsx
@@ -1,13 +1,12 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Col, LinkItem, MainMenu, SubMenu } from "./MenuStyle";
 
-import { Button } from '@mui/material';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Col, LinkItem, MainMenu, SubMenu } from './MenuStyle';
-
-function Menu(){
+function Menu() {
   const [activeMenu, setActiveMenu] = useState(null);
 
-  const handleMouseEnter = (index : any) => {
+  const handleMouseEnter = (index: any) => {
     setActiveMenu(index);
   };
 
@@ -17,17 +16,21 @@ function Menu(){
 
   return (
     <MainMenu>
-      <ul className='main-menu'>
-      <Col>
-          <li 
+      <ul className="main-menu">
+        <Col>
+          <li
             className="main-menu-item"
             onMouseEnter={() => handleMouseEnter(0)}
             onMouseLeave={handleMouseLeave}
           >
             <div>
-              <span className={`menu-title ${activeMenu === 0 ? 'active' : ''}`} >레시피</span>
+              <span
+                className={`menu-title ${activeMenu === 0 ? "active" : ""}`}
+              >
+                레시피
+              </span>
             </div>
-            <SubMenu className='sub-menu'>
+            <SubMenu className="sub-menu">
               <ul>
                 <li>
                   <LinkItem to="/all_recipe">전체</LinkItem>
@@ -41,15 +44,19 @@ function Menu(){
               </ul>
             </SubMenu>
           </li>
-          <li 
+          <li
             className="main-menu-item"
             onMouseEnter={() => handleMouseEnter(1)}
             onMouseLeave={handleMouseLeave}
           >
             <div>
-              <span className={`menu-title ${activeMenu === 1 ? 'active' : ''}`}>보드</span>
+              <span
+                className={`menu-title ${activeMenu === 1 ? "active" : ""}`}
+              >
+                보드
+              </span>
             </div>
-            <SubMenu className='sub-menu'>
+            <SubMenu className="sub-menu">
               <ul>
                 <li>
                   <LinkItem to="/notice">공지사항</LinkItem>
@@ -63,15 +70,19 @@ function Menu(){
               </ul>
             </SubMenu>
           </li>
-          <li 
+          <li
             className="main-menu-item"
             onMouseEnter={() => handleMouseEnter(2)}
-            onMouseLeave={handleMouseLeave} 
+            onMouseLeave={handleMouseLeave}
           >
             <div>
-              <span className={`menu-title ${activeMenu === 2 ? 'active' : ''}`}>마이페이지</span>
+              <span
+                className={`menu-title ${activeMenu === 2 ? "active" : ""}`}
+              >
+                마이페이지
+              </span>
             </div>
-            <SubMenu className='sub-menu'>
+            <SubMenu className="sub-menu">
               <ul>
                 <li>
                   <LinkItem to="/profile">내 정보</LinkItem>
@@ -82,10 +93,30 @@ function Menu(){
               </ul>
             </SubMenu>
           </li>
+          <li
+            className="main-menu-item"
+            onMouseEnter={() => handleMouseEnter(3)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div>
+              <span
+                className={`menu-title ${activeMenu === 3 ? "active" : ""}`}
+              >
+                테스트
+              </span>
+            </div>
+            <SubMenu className="sub-menu">
+              <ul>
+                <li>
+                  <LinkItem to="/test">임시페이지</LinkItem>
+                </li>
+              </ul>
+            </SubMenu>
+          </li>
         </Col>
       </ul>
     </MainMenu>
   );
-};
+}
 
 export default Menu;
