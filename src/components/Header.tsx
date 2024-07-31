63ba502f3ccd3ace2f5119b7acf9dc866ba05ee9
eunhye lee <eeunh2@gmail.com>
2024-07-31 18:39:18 +0900

    #4 전체 검색 영역 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { IconButton, Tooltip } from "@mui/material";
 import Menu from "../memu/Menu";
-import { Col, Logo, MotionInput, MotionSearch, Nav } from "../memu/MenuStyle";
+import { Col, Logo, MotionInput, MotionSearch, Nav, MotionIconButton } from "../memu/MenuStyle";
 import Profile from "./Profile";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
@@ -8,10 +8,20 @@ import SearchIcon from "@mui/icons-material/Search";
 import LoginIcon from "@mui/icons-material/Login";
 import { motion, useAnimation } from "framer-motion";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+
+///임시 검색 인터페이스
+interface IForm {
+  keyword: string;
+}
+
 
 function Header({ isDarkMode, onToggleTheme }: any) {
   const [searchOpen, setSearchOpen] = useState(false);
   const inputAnimation = useAnimation();
+  const navigate = useNavigate();
 
   const toggleSearch = () => {
     if (searchOpen) {
@@ -26,26 +36,47 @@ function Header({ isDarkMode, onToggleTheme }: any) {
     setSearchOpen((prev) => !prev);
   };
 
+  
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data : IForm) => {
+    navigate(`/search?keyword=${data.keyword}`)
+  }
+
   return (
     <Nav>
       <Col>
         <div style={{ display: "flex" }}>
           <Logo>
-            {isDarkMode ? (
-              <img className="logo" src="/logo/dark_logo2.png" alt="logo" />
-            ) : (
-              <img className="logo" src="/logo/light_logo2.png" alt="logo" />
-            )}
+            <a href="/main">
+              {isDarkMode ? (
+                <img className="logo" src="/logo/dark_logo2.png" alt="logo" />
+              ) : (
+                <img className="logo" src="/logo/light_logo2.png" alt="logo" />
+              )}
+            </a>
           </Logo>
           <Menu />
         </div>
         <div style={{ display: "flex" }}>
           <Tooltip title="검색">
-            <SearchIcon
-              className="search-icon"
-              color="primary"
-              onClick={toggleSearch}
-            />
+            <MotionSearch onSubmit={handleSubmit(onValid)}>
+              <MotionIconButton
+                onClick={toggleSearch}
+                animate={{ x: searchOpen ? -215 : 0 }}
+                transition={{ type: 'linear' }}
+              >
+                <IconButton className="search-icon">
+                  <SearchIcon color="primary" />
+                </IconButton>
+              </MotionIconButton>
+              <MotionInput
+                {...register("keyword", { required: true, minLength: 2 })}
+                initial={{ scaleX: 0 }}
+                animate={inputAnimation}
+                transition={{ type: "linear" }}
+                placeholder="검색어를 입력해주세요."
+              />
+            </MotionSearch>
           </Tooltip>
           <Tooltip title="모드 변경">
             <IconButton
