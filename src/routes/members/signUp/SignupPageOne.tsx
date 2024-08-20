87df0295dbdcc6c79f89693d83ad133c3aa53d28
dev-jieun-lee <dev.jieun.lee@gmail.com>
2024-08-20 13:50:46 +0900

    회원가입 시 중복전화번호 가입 방지

--- src/routes/members/signUp/SignupPageOne.tsx
+++ src/routes/members/signUp/SignupPageOne.tsx
@@ -4,8 +4,9 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { LoginWrapper } from "../login/LoginStyle";
 import { Wrapper } from "../../../styles/CommonStyles";
-import { Button, Checkbox, FormControlLabel, TextField, FormHelperText } from "@mui/material";
+import { Button, Checkbox, FormControlLabel, TextField, FormHelperText, FormGroup } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function SignupPageOne() {
   const { t } = useTranslation();
@@ -24,9 +25,28 @@ function SignupPageOne() {
       agreePrivacy: Yup.boolean().oneOf([true], t('members.privacy_required')),
       agreeMarketing: Yup.boolean().oneOf([true], t('members.marketing_required'))
     }),
-    onSubmit: (values, { setSubmitting }) => {
-      setSubmitting(false);
-      navigate('/signup/signupPageTwo', { state: { phoneNumber: values.phone } });
+    onSubmit: async (values, { setSubmitting }) => {
+      setSubmitting(true);
+
+      try {
+        // 핸드폰 번호 유효성 검사 API 호출
+        const response = await axios.post('/api/members/check-phone', { phone: values.phone });
+        console.log(response.data);
+        if (response.data === false) {
+          // 유효한 경우 페이지 전환
+          navigate('/signup/signupPageTwo', { state: { phone: values.phone } });
+        } else {
+          // 유효하지 않은 경우 에러 처리
+          formik.setFieldError('phone', t('members.phone_number_invalid'));
+        }
+      } catch (error) {
+        // API 호출 오류 처리
+        console.error('API 호출 오류:', error);
+        // 서버 오류 메시지 표시
+        formik.setFieldError('phone', t('members.phone_number_error'));
+      } finally {
+        setSubmitting(false);
+      }
     }
   });
 
@@ -50,29 +70,31 @@ function SignupPageOne() {
             margin="normal"
           />
 
-          <FormControlLabel
-            control={<Checkbox name="agreeTerms" checked={formik.values.agreeTerms} onChange={formik.handleChange} />}
-            label={t('members.agree_terms')}
-          />
-          {formik.touched.agreeTerms && formik.errors.agreeTerms && (
-            <FormHelperText error>{formik.errors.agreeTerms}</FormHelperText>
-          )}
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox name="agreeTerms" checked={formik.values.agreeTerms} onChange={formik.handleChange} />}
+              label={t('members.agree_terms')}
+            />
+            {formik.touched.agreeTerms && formik.errors.agreeTerms && (
+              <FormHelperText error>{formik.errors.agreeTerms}</FormHelperText>
+            )}
 
-          <FormControlLabel
-            control={<Checkbox name="agreePrivacy" checked={formik.values.agreePrivacy} onChange={formik.handleChange} />}
-            label={t('members.agree_privacy')}
-          />
-          {formik.touched.agreePrivacy && formik.errors.agreePrivacy && (
-            <FormHelperText error>{formik.errors.agreePrivacy}</FormHelperText>
-          )}
+            <FormControlLabel
+              control={<Checkbox name="agreePrivacy" checked={formik.values.agreePrivacy} onChange={formik.handleChange} />}
+              label={t('members.agree_privacy')}
+            />
+            {formik.touched.agreePrivacy && formik.errors.agreePrivacy && (
+              <FormHelperText error>{formik.errors.agreePrivacy}</FormHelperText>
+            )}
 
-          <FormControlLabel
-            control={<Checkbox name="agreeMarketing" checked={formik.values.agreeMarketing} onChange={formik.handleChange} />}
-            label={t('members.agree_marketing')}
-          />
-          {formik.touched.agreeMarketing && formik.errors.agreeMarketing && (
-            <FormHelperText error>{formik.errors.agreeMarketing}</FormHelperText>
-          )}
+            <FormControlLabel
+              control={<Checkbox name="agreeMarketing" checked={formik.values.agreeMarketing} onChange={formik.handleChange} />}
+              label={t('members.agree_marketing')}
+            />
+            {formik.touched.agreeMarketing && formik.errors.agreeMarketing && (
+              <FormHelperText error>{formik.errors.agreeMarketing}</FormHelperText>
+            )}
+          </FormGroup>
 
           <Button color="primary" variant="contained" fullWidth type="submit">
             {t('members.verify_continue')}
