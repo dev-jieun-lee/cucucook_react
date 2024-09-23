f4a2857a854da3270fe6c28239400a9c9d73d1a5
unknown <eeunh2@gmail.com>
2024-09-23 20:57:10 +0900

    메인화면 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import { MenuItem, Select } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import i18n from "../locales/i18n";
+import media from "../styles/MediaQuery";
 
 function Footer({ isDarkMode }: any) {
   const { t, i18n } = useTranslation(); // i18next 훅 사용
