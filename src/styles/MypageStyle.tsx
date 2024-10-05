9ec1abcf625ddf1a69a709be9e2fd5ad9ea00f54
unknown <eeunh2@gmail.com>
2024-10-05 16:14:12 +0900

    내 활동/ 내 댓글 디자인 수정 중간커밋

--- src/styles/MypageStyle.tsx
+++ src/styles/MypageStyle.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import styled from "styled-components";
+import media from "./MediaQuery";
 
 export const SubTitle = styled.div`
   margin-top: 50px;
@@ -76,8 +77,9 @@ export const MyPageTitle = styled.div`
   color: ${(props) => props.theme.mainColor};
   font-weight: bold;
   font-size: 1.6rem;
-  span{
-  }
+  ${media.small`
+    font-size: 1.4rem;
+  `};
 `;
 
 export const SummaryCountArea = styled.div`
@@ -86,7 +88,10 @@ export const SummaryCountArea = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 200px;
   margin-top: 50px;
-  /* gap: 20px; */
+  ${media.small`
+    grid-template-rows: 150px;
+    margin-top: 30px;
+  `};
   .grid{
     &:nth-child(2){
       border-right: 1px solid;
@@ -97,15 +102,25 @@ export const SummaryCountArea = styled.div`
     transform: scale(2);
     margin-top: 25px;
     color: ${(props) => props.theme.textColor};
+    ${media.small`
+      margin-top: 15px;
+      transform: scale(1.5);
+    `};
   }
   .kind{
     font-size: 1.2rem;
     color: ${(props) => props.theme.textColor};
+    ${media.small`
+      font-size: 1.1rem;
+    `};
   }
   .count{
     font-size: 1.8rem;
     font-weight: bold;
     color: ${(props) => props.theme.mainColor};
+    ${media.small`
+      font-size: 1.5rem;
+    `};
   }
 `;
 
@@ -116,6 +131,10 @@ export const SummaryDataArea = styled.div`
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, 330px);
   gap: 10px;
+  ${media.medium`
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, 330px);
+  `};
   .grid{
     border: 1px solid;
     border-color: ${(props) => props.theme.navBorderColor};
@@ -146,7 +165,7 @@ export const SummaryDataArea = styled.div`
     .more-icon{
       transform: scale(0.7);
       color: #898787d3;
-      margin-top: 5px;
+      margin-top: 10px;
     }
     .row {
         &:hover {
@@ -159,4 +178,9 @@ export const SummaryDataArea = styled.div`
       }
   }
 
+`;
+
+
+export const MypageContentArea = styled.div`
+  width: 100%;
 `;
