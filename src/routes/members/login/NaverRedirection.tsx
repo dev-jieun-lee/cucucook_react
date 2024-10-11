778d78917fd60a76df376ea6ce8fbad0f9ba7c6c
haerim <gofla333@naver.com>
2024-10-12 01:59:01 +0900

    sns 로그인 후 로그인하기

--- src/routes/members/login/NaverRedirection.tsx
+++ src/routes/members/login/NaverRedirection.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { handleNaverCallback } from "../../../apis/socialLoginApi";
+import { useAuth } from "../../../auth/AuthContext";
 
 const NaverCallback = () => {
   const navigate = useNavigate();
+  const { setUser, setLoggedIn } = useAuth();
 
   useEffect(() => {
     const code = new URLSearchParams(window.location.search).get("code");
@@ -14,8 +16,16 @@ const NaverCallback = () => {
           const data = await handleNaverCallback(code);
           // 이후 사용자 정보를 처리 (예: 로그인 상태 업데이트)
           console.log("사용자 정보:", data);
+          setUser({
+            userId: data.member.userId,
+            name: data.member.name,
+            role: data.member.role,
+            memberId: data.member.memberId,
+          });
+
+          setLoggedIn(true);
           // 로그인 성공 후 리다이렉트
-          navigate("/main"); // 원하는 페이지로 리다이렉트
+          navigate("/"); // 원하는 페이지로 리다이렉트
         } catch (error) {
           console.error("로그인 처리 중 오류 발생:", error);
           // 오류 처리 로직 추가 (예: 오류 메시지 표시)
