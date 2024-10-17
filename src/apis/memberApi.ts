e1fe3e8396e424bc7ef14e65df5e81150e99bfe0
dev-jieun-lee <dev.jieun.lee@gmail.com>
2024-10-17 21:28:08 +0900

    회원가입시 메일중복확인

--- src/apis/memberApi.ts
+++ src/apis/memberApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AxiosError } from "axios";
 import Cookies from "js-cookie"; // js-cookie 라이브러리 추가
 import { useMutation } from "react-query";
 
@@ -13,7 +14,6 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
 
 // 로그인 요청
 export async function login(form: {
@@ -30,7 +30,6 @@ export async function login(form: {
     } else {
       console.error("알 수 없는 오류 발생:", error);
     }
-
   }
 }
 
@@ -40,8 +39,7 @@ export async function logout() {
     const response = await api.post("/logout");
 
     return response.data;
-  } catch (error) {
-  }
+  } catch (error) {}
 }
 
 // 핸드폰번호 중복체크
@@ -84,23 +82,37 @@ export const findId = async (data: {
 };
 
 // 이메일 인증 코드 발송
-export const useSendEmailVerificationCode = () =>
-  useMutation((email: string) =>
-    api
-      .post("/sendVerificationCode", { email })
-      .then((response) => {
+export const useSendEmailVerificationCode = () => {
+  return useMutation(
+    (email: string) =>
+      api.post("/sendVerificationCode", { email }).then((response) => {
         return response.data;
-      })
+      }),
+    {
+      onSuccess: () => {
+        alert("인증 코드가 이메일로 발송되었습니다.");
+      },
+      onError: (error: unknown) => {
+        if (error instanceof AxiosError) {
+          if (error.response && error.response.status === 409) {
+            alert("이미 가입된 이메일입니다.");
+          } else {
+            alert("이메일 전송에 실패했습니다. 다시 시도해 주세요.");
+          }
+        } else {
+          alert("알 수 없는 오류가 발생했습니다.");
+        }
+      },
+    }
   );
+};
 
 // 이메일 인증 코드 검증
 export const useVerifyEmailCode = () =>
   useMutation(({ email, code }: { email: string; code: string }) =>
-    api
-      .post("/verify", { email, code })
-      .then((response) => {
-        return response.data;
-      })
+    api.post("/verify", { email, code }).then((response) => {
+      return response.data;
+    })
   );
 
 // 이메일 중복 체크 API
@@ -135,8 +147,7 @@ export async function validateToken(token: string) {
   try {
     const response = await api.post("/validateToken", { token });
     return response.data; // { valid: boolean } 형태의 데이터 반환
-  } catch (error) {
-  }
+  } catch (error) {}
 }
 
 // 이메일, 이름, ID로 사용자 존재 여부 확인 API
